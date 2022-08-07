f847d0363480f924467c2246dedeff9bd23f7ff8
yogipristiawan <yogistanada252@gmail.com>
2022-08-07 22:29:27 +0700

    fix: validate available car when creating rental data

--- proses_tambah.php
+++ proses_tambah.php
@@ -2,47 +2,31 @@
 include "koneksi.php";
 
 $kd_mobil = $_POST['kd_mobil'];
-$kd_customer = $_POST['kd_customer'];
+$kd_customer = (int)$_POST['kd_customer'];
 $tgl_pinjam = $_POST['tgl_pinjam'];
 $tgl_kembali = $_POST['tgl_kembali'];
 
-// var_dump(mysqli_error($koneksi));
-// die;
-// var_dump($kd_mobil);
-// var_dump(mysqli_fetch_assoc($querymobil));
-// die;
-$querymobil = mysqli_query($koneksi, "SELECT * FROM mobil WHERE kd_mobil = '$kd_mobil'");
-if (mysqli_num_rows($querymobil)) {
-    while ($mobil = mysqli_fetch_assoc($querymobil)) {
-        $stok = $mobil['stok'];
-    }
-}
+$getCarsByID = mysqli_query($koneksi, "SELECT * FROM mobil WHERE kd_mobil = '$kd_mobil'");
 
-
-if ($stok == 0) {
-    // echo "ga ke kirim?";
-    // echo "<script>alert('Stok Kosong, Pilih Mobil Lain!'); location.href = 'tambah.php'; </script>";
-} else if ($tgl_pinjam > $tgl_kembali) {
+if (!mysqli_num_rows($getCarsByID))
+    echo "<script>alert('Stok Kosong, Pilih Mobil Lain!'); location.href = 'tambah.php'; </script>";
+else if ($tgl_pinjam > $tgl_kembali)
     echo "<script>alert('Format Tanggal Salah!'); location.href = 'tambah.php'; </script>";
-} else {
+else {
 
     $diff = date_diff(date_create($tgl_pinjam), date_create($tgl_kembali));
     $hari = $diff->format("%a") + 1;
-    $sql = "SELECT * FROM mobil WHERE kd_mobil = '$kd_mobil'";
-    $query = mysqli_query($koneksi, $sql);
 
-    while ($mobil = mysqli_fetch_assoc($query)) {
+    while ($mobil = mysqli_fetch_assoc($getCarsByID)) {
         $tarif_sewa = $mobil['tarif_sewa'];
     }
 
     $total_sewa = $hari * $tarif_sewa;
 
-    $sql1 = "INSERT INTO sewa(kd_mobil, kd_customer, tgl_pinjam, tgl_kembali, total_sewa) VALUES('$kd_mobil', '$kd_customer', '$tgl_pinjam', '$tgl_kembali', '$total_sewa')";
-    $query1 = mysqli_query($koneksi, $sql1);
-
-    if ($query1) {
+    $createRent = "INSERT INTO sewa(kd_mobil, kd_customer, tgl_pinjam, tgl_kembali, total_sewa) VALUES('$kd_mobil', $kd_customer, '$tgl_pinjam', '$tgl_kembali', '$total_sewa')";
+    $resultRent = mysqli_query($koneksi, $createRent);
+    if ($resultRent)
         echo "<script>alert('Tambah Data Berhasil!'); location.href = 'index.php'; </script>";
-    } else {
+    else
         echo "<script>alert('Tambah Data Gagal!'); location.href = 'tambah.php'; </script>";
-    }
 }
